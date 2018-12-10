df543ff8be611ba4fd29ba8d5bc7bbb53f4c0112
Troy Unverdruss <troy.unverdruss@vevo.com>
2018-12-10 09:34:53 -0800

    Day 9, part 1 & 2, refactored to be MUCH faster than initial solution

--- day09/test_solve_9.py
+++ day09/test_solve_9.py
@@ -1,36 +1,36 @@
 from unittest import TestCase
-from day09.puzzle_9 import solve_9a, Circle
+from day09.puzzle_9 import solve_9, Circle
 
 
 class TestSolve9(TestCase):
     def test_1(self):
         input = '10 players; last marble is worth 1618 points'
-        r = solve_9a(input)
+        r = solve_9(input)
         self.assertEqual(8317, r)
 
     def test_2(self):
         input = '13 players; last marble is worth 7999 points'
-        r = solve_9a(input)
+        r = solve_9(input)
         self.assertEqual(146373, r)
 
     def test_3(self):
         input = '17 players; last marble is worth 1104 points'
-        r = solve_9a(input)
+        r = solve_9(input)
         self.assertEqual(2764, r)
 
     def test_4(self):
         input = '21 players; last marble is worth 6111 points'
-        r = solve_9a(input)
+        r = solve_9(input)
         self.assertEqual(54718, r)
 
     def test_5(self):
         input = '30 players; last marble is worth 5807 points'
-        r = solve_9a(input)
+        r = solve_9(input)
         self.assertEqual(37305, r)
 
     def test_6(self):
         input = '9 players; last marble is worth 25 points'
-        r = solve_9a(input)
+        r = solve_9(input)
         self.assertEqual(32, r)
 
     def test_subtract_7_over_0(self):
@@ -71,31 +71,31 @@ class TestSolve9(TestCase):
     def test_board_matches(self):
         expected_board_entries = [
             '(0) ',
-            '0 (1) ',
-            '0 (2) 1 ',
-            '0 2 1 (3) ',
-            '0 (4) 2 1 3 ',
-            '0 4 2 (5) 1 3 ',
-            '0 4 2 5 1 (6) 3 ',
-            '0 4 2 5 1 6 3 (7) ',
-            '0 (8) 4 2 5 1 6 3 7 ',
-            '0 8 4 (9) 2 5 1 6 3 7 ',
-            '0 8 4 9 2 (10) 5 1 6 3 7 ',
-            '0 8 4 9 2 10 5 (11) 1 6 3 7 ',
-            '0 8 4 9 2 10 5 11 1 (12) 6 3 7 ',
-            '0 8 4 9 2 10 5 11 1 12 6 (13) 3 7 ',
-            '0 8 4 9 2 10 5 11 1 12 6 13 3 (14) 7 ',
-            '0 8 4 9 2 10 5 11 1 12 6 13 3 14 7 (15) ',
-            '0 (16) 8 4 9 2 10 5 11 1 12 6 13 3 14 7 15 ',
-            '0 16 8 (17) 4 9 2 10 5 11 1 12 6 13 3 14 7 15 ',
-            '0 16 8 17 4 (18) 9 2 10 5 11 1 12 6 13 3 14 7 15 ',
-            '0 16 8 17 4 18 9 (19) 2 10 5 11 1 12 6 13 3 14 7 15 ',
-            '0 16 8 17 4 18 9 19 2 (20) 10 5 11 1 12 6 13 3 14 7 15 ',
-            '0 16 8 17 4 18 9 19 2 20 10 (21) 5 11 1 12 6 13 3 14 7 15 ',
-            '0 16 8 17 4 18 9 19 2 20 10 21 5 (22) 11 1 12 6 13 3 14 7 15 ',
-            '0 16 8 17 4 18 (19) 2 20 10 21 5 22 11 1 12 6 13 3 14 7 15 ',
-            '0 16 8 17 4 18 19 2 (24) 20 10 21 5 22 11 1 12 6 13 3 14 7 15 ',
-            '0 16 8 17 4 18 19 2 24 20 (25) 10 21 5 22 11 1 12 6 13 3 14 7 15 '
+            '(1) 0 ',
+            '(2) 1 0 ',
+            '(3) 0 2 1 ',
+            '(4) 2 1 3 0 ',
+            '(5) 1 3 0 4 2 ',
+            '(6) 3 0 4 2 5 1 ',
+            '(7) 0 4 2 5 1 6 3 ',
+            '(8) 4 2 5 1 6 3 7 0 ',
+            '(9) 2 5 1 6 3 7 0 8 4 ',
+            '(10) 5 1 6 3 7 0 8 4 9 2 ',
+            '(11) 1 6 3 7 0 8 4 9 2 10 5 ',
+            '(12) 6 3 7 0 8 4 9 2 10 5 11 1 ',
+            '(13) 3 7 0 8 4 9 2 10 5 11 1 12 6 ',
+            '(14) 7 0 8 4 9 2 10 5 11 1 12 6 13 3 ',
+            '(15) 0 8 4 9 2 10 5 11 1 12 6 13 3 14 7 ',
+            '(16) 8 4 9 2 10 5 11 1 12 6 13 3 14 7 15 0 ',
+            '(17) 4 9 2 10 5 11 1 12 6 13 3 14 7 15 0 16 8 ',
+            '(18) 9 2 10 5 11 1 12 6 13 3 14 7 15 0 16 8 17 4 ',
+            '(19) 2 10 5 11 1 12 6 13 3 14 7 15 0 16 8 17 4 18 9 ',
+            '(20) 10 5 11 1 12 6 13 3 14 7 15 0 16 8 17 4 18 9 19 2 ',
+            '(21) 5 11 1 12 6 13 3 14 7 15 0 16 8 17 4 18 9 19 2 20 10 ',
+            '(22) 11 1 12 6 13 3 14 7 15 0 16 8 17 4 18 9 19 2 20 10 21 5 ',
+            '(19) 2 20 10 21 5 22 11 1 12 6 13 3 14 7 15 0 16 8 17 4 18 ',
+            '(24) 20 10 21 5 22 11 1 12 6 13 3 14 7 15 0 16 8 17 4 18 19 2 ',
+            '(25) 10 21 5 22 11 1 12 6 13 3 14 7 15 0 16 8 17 4 18 19 2 24 20 ',
         ]
 
         circle = Circle()
@@ -104,9 +104,8 @@ class TestSolve9(TestCase):
         while marble_value <= 25:
             print('marble value: {}'.format(marble_value))
 
-            score, string_rep = circle.add_marble(marble_value)
+            score, string_rep = circle.add_marble(marble_value, compute_board_str=True)
             print(string_rep)
 
-            self.assertEqual(string_rep, expected_board_entries[marble_value])
+            self.assertEqual(expected_board_entries[marble_value], string_rep)
             marble_value += 1
-
