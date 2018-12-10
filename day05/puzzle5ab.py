99bfb28aa0860d34bf7e7c52c6caaedddf302f99
Troy Unverdruss <troy@unverdruss.net>
2018-12-09 18:15:46 -0800

    Fiddling with day 5 to see if I could make it run faster

--- day05/puzzle5ab.py
+++ day05/puzzle5ab.py
@@ -18,6 +18,28 @@ def solve_5a(polymer):
     return ''.join(atoms)
 
 
+def solve_try_1(polymer: str):
+    first = True
+    regex = ''
+    for l in list(string.ascii_lowercase):
+        if not first:
+            regex += '|'
+        first = False
+
+        c1 = l
+        c2 = l.swapcase()
+        regex += '{}{}|{}{}'.format(c1, c2, c2, c1)
+
+    p = re.compile(regex)
+
+    modified = True
+    while modified:
+        new_polymer = re.sub(p, '', polymer)
+        modified = new_polymer != polymer
+        polymer = new_polymer
+
+    return polymer
+
 def solve_5b(polymer):
     letters = list(string.ascii_lowercase)
 
@@ -27,7 +49,7 @@ def solve_5b(polymer):
         print(letter)
         stripped = re.sub(r'[{}{}]'.format(letter, letter.swapcase()), '', polymer)
         print('{} stripped length: {}'.format(letter, len(stripped)))
-        compressed_polymer_length = len(solve_5a(stripped))
+        compressed_polymer_length = len(solve_try_1(stripped))
         if (compressed_polymer_length) < shortest_polymer:
             shortest_polymer = compressed_polymer_length
 
@@ -35,7 +57,7 @@ def solve_5b(polymer):
 
 if __name__ == '__main__':
     polymer = read_raw_entries('input.txt')[0]
-    result = solve_5a(polymer)
+    result = solve_try_1(polymer)
     print('Compressed polymer length: {}'.format(len(result)))
 
     polymer = read_raw_entries('input.txt')[0]
--- day05/test_puzzle_5a.py
+++ day05/test_puzzle_5a.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
-from day05.puzzle5ab import solve_5a, solve_5b
-
+from helpers.helpers import read_raw_entries
+from day05.puzzle5ab import solve_5a, solve_5b, solve_try_1
+import timeit
 
 class TestSolve_5a(TestCase):
     def test_5a_1(self):
@@ -27,3 +28,31 @@ class TestSolve_5a(TestCase):
 
         self.assertEqual(4, r)
 
+    def test_perf(self):
+        t = timeit.timeit(self.test_solve_5a, number=1000)
+        print('Initial solution took: {}'.format(t))
+
+        t = timeit.timeit(self.solve_try_1, number=1000)
+        print('Time: {}'.format(t))
+
+    def test_perf_2(self):
+        t = timeit.timeit(self.do_work_1, number=1)
+        print('Initial solution took: {}'.format(t))
+
+        t = timeit.timeit(self.do_work_2, number=1)
+        print('Time: {}'.format(t))
+
+    def do_work_1(self):
+        polymer = read_raw_entries('input.txt')[0]
+        result = solve_5a(polymer)
+
+    def do_work_2(self):
+        polymer = read_raw_entries('input.txt')[0]
+        result = solve_try_1(polymer)
+
+
+    def solve_try_1(self):
+        polymer = 'dabAcCaCBAcCcaDA'
+        r = solve_try_1(polymer)
+
+        self.assertEqual('dabCBAcaDA', r)
