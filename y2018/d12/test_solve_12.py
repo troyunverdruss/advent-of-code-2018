a90084f5c1371cb274da6b2764715dd7c0a7c6d5
troy <troy@unverdrus.net>
2018-12-13 09:48:08 -0800

    Preparing to debug bad puzzle 12 solution

--- y2018/d12/test_solve_12.py
+++ y2018/d12/test_solve_12.py
@@ -1,13 +1,17 @@
 from unittest import TestCase
 from helpers.helpers import read_raw_entries
-from y2018.d12.puzzle_12 import solve_12
+from y2018.d12.puzzle_12 import solve_12 as solve_12_bad
+from y2018.d12.puzzle_12_try_2 import solve_12 as solve_12_good
 
 
 class TestSolve_12(TestCase):
     def test_example_1(self):
         entries = read_raw_entries('test-input.txt')
-        r = solve_12(entries)
+        r = solve_12_bad(entries)
         self.assertEqual(325, r)
+
+    def test_debug_code_1(self):
+        pass
 
 
 # ...#..#.#..##......###...###...........
