def38fc7021ae3e74b81bcf3ea05bce7dfccb0c0
Troy Unverdruss <troy@unverdruss.net>
2018-12-15 23:05:52 -0800

    Got part of day 15 working

--- y2018/d15/puzzle_15.py
+++ y2018/d15/puzzle_15.py
@@ -1,6 +1,8 @@
 from helpers.helpers import read_raw_entries, Point, manhattan_distance
 from copy import deepcopy
+import sys
 
+BEST_PATH = None
 
 class Fighter:
     def __init__(self, x, y, type):
@@ -42,8 +44,15 @@ class Fighter:
         if best_path_to_closest_enemy is not None:
             self.loc = best_path_to_closest_enemy[1]
 
-    def attack(self, elves, goblins):
-        pass
+    def attack(self, fighters):
+        potential_locations = []
+        for d in directions:
+            potential_locations.append(self.loc + d)
+
+        potential_enemies = filter(lambda f: f.loc in potential_locations and self.enemy(f), fighters)
+        enemies = sorted(potential_enemies, key=lambda f: f.hit_points)
+        if len(enemies) > 0:
+            enemies[0].hit_points -= 3
 
     def enemy(self, other):
         return self.type != other.type
@@ -77,6 +86,10 @@ def print_state(round, grid_map, elves, goblins):
         for v in row:
             print(v, end='')
         print('')
+    print('')
+
+    for f in elves + goblins:
+        print(f)
 
     print('')
 
@@ -120,11 +133,20 @@ def find_dest_point(fighter: Fighter, filled_map, fighters):
 
 
 # First item in a computed path is always your starting square!
-def compute_best_path(start: Point, dest: Point, filled_map, visited):
+def compute_best_path(start: Point, dest: Point, filled_map, visited, max_length=100):
     if len(visited) == 0:
         # This is a special case since we always start out where there is something!
         pass
     elif filled_map[start.y][start.x] != '.' or start in visited:
+        return None
+
+    if len(visited) == 1:
+        # Let's just make sure that we can reach our destination from here, the first legit non-start point
+        aa = find_accessible_areas_from_point(start, filled_map, [])
+        if dest not in aa:
+            return None
+
+    if len(visited) > max_length:
         return None
 
     visited.append(start)
@@ -137,18 +159,22 @@ def compute_best_path(start: Point, dest: Point, filled_map, visited):
     prioritized_directions = []
     other_directions = []
     absolute_distance = manhattan_distance(start, dest)
+
     for d in directions:
         if manhattan_distance(start + d, dest) <= absolute_distance:
             prioritized_directions.append(d)
         else:
             other_directions.append(d)
 
-    directions_to_try = prioritized_directions
-    if len(directions_to_try) == 0:
-        directions_to_try = other_directions
+    # directions_to_try = prioritized_directions
+    # if len(directions_to_try) == 0:
+    #     directions_to_try = other_directions
 
-    for d in directions_to_try:
-        path = compute_best_path(start + d, dest, filled_map, deepcopy(visited))
+    for d in prioritized_directions:
+        if shortest_path is not None:
+            max_length = len(shortest_path)
+
+        path = compute_best_path(start + d, dest, filled_map, deepcopy(visited), max_length)
 
         if not path:
             continue
@@ -157,12 +183,33 @@ def compute_best_path(start: Point, dest: Point, filled_map, visited):
             shortest_path = path
         elif len(path) < len(shortest_path):
             shortest_path = path
+        elif len(path) == len(shortest_path):
+            if shortest_path[1] != select_by_reading_order(shortest_path[1], path[1]):
+                shortest_path = path
+
+    for d in other_directions:
+        if shortest_path is not None:
+            max_length = len(shortest_path)
+
+        path = compute_best_path(start + d, dest, filled_map, deepcopy(visited), max_length)
+
+        if not path:
+            continue
+
+        if shortest_path is None:
+            shortest_path = path
+        elif len(path) < len(shortest_path):
+            shortest_path = path
+        elif len(path) == len(shortest_path):
+            if shortest_path[1] != select_by_reading_order(shortest_path[1], path[1]):
+                shortest_path = path
 
     return shortest_path
 
 
 def select_by_reading_order(p1: Point, p2: Point):
     return sorted([p1, p2], key=lambda p: (p.y, p.x))[0]
+
 
 def filter_possible_destinations(accessible_areas, possible_destinations):
     temp_pds = []
@@ -190,9 +237,11 @@ def find_possible_destinations(me, filled_map, enemies):
 
 
 def find_accessible_areas_from_point(start: Point, filled_map, already_known):
+    if start not in already_known:
+        already_known.append(start)
     for d in directions:
         test = start + d
-        if filled_map[test.y][test.x] == '.':
+        if test.x >= 0 and test.y >= 0 and test.x < len(filled_map[0]) and test.y < len(filled_map) and filled_map[test.y][test.x] == '.':
             new_point = Point(test.x, test.y)
             if new_point not in already_known:
                 already_known.append(new_point)
@@ -213,12 +262,26 @@ def solve_15(input):
     while combat:
         for fighter in sort_fighters(elves, goblins):
             fighter.move(grid_map, elves + goblins)
-            fighter.attack(elves, goblins)
+            print('.', end='')
+            sys.stdout.flush()
+            fighter.attack(elves + goblins)
+
+            elves = list(filter(lambda e: e.hit_points > 0, elves))
+            goblins = list(filter(lambda g: g.hit_points > 0, goblins))
+
+            if len(elves) == 0 or len(goblins) == 0:
+                combat = False
+
+        print_state(round, grid_map, elves, goblins)
 
         if combat:
             round += 1
 
-    i = 0
+    hit_points = 0
+    for f in elves + goblins:
+        hit_points += f.hit_points
+
+    return round * hit_points
 
 
 def sort_fighters(elves, goblins):
