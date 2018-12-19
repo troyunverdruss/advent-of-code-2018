e6786e934759b23dd0aa345611a721f18c163f9c
Troy Unverdruss <troy.unverdruss@vevo.com>
2018-12-19 12:58:00 -0800

    Test case passes for puzzle 17

---
+++ y2018/d17/test_solve_17.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+
+from y2018.d17.puzzle_17 import solve_17
+
+
+class TestSolve_17(TestCase):
+    def test_input(self):
+        r = solve_17('test-input.txt')
+        self.assertEqual(57, r)
+
+    def test_edge_case(self):
+        r = solve_17('edge-case-test.txt')
+        self.assertEqual(57, r)
