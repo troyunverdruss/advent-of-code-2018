d35093fb5b230250bcd04f10f1def8f7cb26a8ed
Troy Unverdruss <troy.unverdruss@vevo.com>
2018-12-20 16:36:55 -0800

    Puzzle 18 part 1 done

---
+++ y2018/d18/test_solve_18.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+from helpers.helpers import read_raw_entries
+from y2018.d18.puzzle_18 import solve_18
+
+
+class TestSolve_18(TestCase):
+    def test_sample(self):
+        entries = read_raw_entries('test-input.txt')
+        r = solve_18(entries)
+        self.assertEqual(1147, r)
