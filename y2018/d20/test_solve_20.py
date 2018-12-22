d70e6e9c4b494e07408973cac21126d59e8d94e4
Troy Unverdruss <troy.unverdruss@vevo.com>
2018-12-21 18:52:52 -0800

    puzzle 20, all test cases passing now

--- y2018/d20/test_solve_20.py
+++ y2018/d20/test_solve_20.py
@@ -15,3 +15,7 @@ class TestSolve_20(TestCase):
     def test_sample_3(self):
         d = solve_20('^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$')
         self.assertEqual(18, d)
+
+    def test_mine_1(self):
+        d = solve_20('^N(NES|)SS$')
+        self.assertEqual(6, d)
