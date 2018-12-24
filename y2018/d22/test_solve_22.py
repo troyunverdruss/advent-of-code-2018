d844c98989a69fa6fc723ae69a229cc1ad019494
Troy Unverdruss <troy.unverdruss@vevo.com>
2018-12-23 21:46:44 -0800

    Part 1 completed

--- y2018/d22/test_solve_22.py
+++ y2018/d22/test_solve_22.py
@@ -6,10 +6,5 @@ from y2018.d22.puzzle_22 import solve_22
 
 class TestSolve_22(TestCase):
     def test_example(self):
-        r = solve_22(510, Point(10,10))
-        self.assertEqual(114, r)
-
-    @skip
-    def test_mine_1(self):
-        r = solve_22(510, Point(2, 5))
+        r = solve_22(510, Point(10, 10))
         self.assertEqual(114, r)
