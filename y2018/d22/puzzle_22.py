dac01a8428da22a0ef4184a16da4250c7aa4454b
Troy Unverdruss <troy.unverdruss@vevo.com>
2018-12-24 10:42:40 -0800

    Puzzle 22 part 1 refactor and part 2 done

--- y2018/d22/puzzle_22.py
+++ y2018/d22/puzzle_22.py
@@ -1,7 +1,24 @@
 from helpers.helpers import Point, read_raw_entries
+import networkx as nx
+
+
+class RegionNode:
+    def __init__(self, region, loc, tool):
+        self.region = region
+        self.loc = loc
+        self.tool = tool
+
+    def __repr__(self):
+        return repr('({}, {}), {}'.format(self.loc.x, self.loc.y, self.tool))
 
 
 class Region:
+    _allowed_tools = {
+        0: ['C', 'T'],  # Rocky: climbing, torch
+        1: ['C', 'N'],  # Wet: climbing, neither
+        2: ['T', 'N'],  # Narrow: torch, neither
+    }
+
     def __init__(self, loc, target, cave, depth):
         self.loc = loc
         self.target = target
@@ -12,16 +29,17 @@ class Region:
         self._erosion_level = None
         self._type = None
 
-        self._building = True
         self.type()
-        self._building = False
+
+        self.nodes = {
+            self.allowed_tools()[0]: RegionNode(self, self.loc, self.allowed_tools()[0]),
+            self.allowed_tools()[1]: RegionNode(self, self.loc, self.allowed_tools()[1]),
+        }
 
     def geo_index(self):
         if self._geo_index is not None:
             return self._geo_index
 
-        if not self._building:
-            print('Cache miss for {} (geo_index)'.format(self.loc))
         if self.loc == Point(0, 0):
             self._geo_index = 0
         elif self.loc == self.target:
@@ -41,8 +59,6 @@ class Region:
         if self._erosion_level is not None:
             return self._erosion_level
 
-        if not self._building:
-            print('Cache miss for {} (erosion_level)'.format(self.loc))
         self._erosion_level = (self.geo_index() + self.depth) % 20183
         return self._erosion_level
 
@@ -50,28 +66,16 @@ class Region:
         if self._type is not None:
             return self._type
 
-        if not self._building:
-            print('Cache miss for {} (type)'.format(self.loc))
         self._type = self.erosion_level() % 3
         # print('Computed type for {}: {}'.format(self.loc, self._type))
 
         return self._type
 
-    def display_type(self):
-        if self.type() == 0:
-            # Rocky
-            self._type = '.'
-        elif self.type() == 1:
-            # Wet
-            self._type = '='
-        elif self.type() == 2:
-            # Narrow
-            self._type = '|'
-        else:
-            Exception('Unknown type: {}'.format(self.type()))
+    def allowed_tools(self):
+        return Region._allowed_tools[self.type()]
 
 
-def solve_22(depth, target):
+def solve_22_part_1(depth, target):
     cave = {}
     risk = 0
     for x in range(0, target.x + 1):
@@ -84,10 +88,39 @@ def solve_22(depth, target):
     return risk
 
 
+def solve_22_part_2(depth, target):
+    cave = {}
+    for x in range(0, target.x + 50):
+        for y in range(0, target.y + 50):
+            region = Region(Point(x, y), target, cave, depth)
+            cave[Point(x, y)] = region
+
+    g = nx.Graph()
+
+    for k, region in cave.items():
+        allowed_tools = region.allowed_tools()
+        g.add_edge(region.nodes[allowed_tools[0]], region.nodes[allowed_tools[1]], weight=7)
+
+        for direction in [Point(-1, 0), Point(0, -1)]:
+            if (region.loc.x == 0 and direction.x == -1) or (region.loc.y == 0 and direction.y == -1):
+                continue
+
+            neighbor = cave[region.loc + direction]
+            for tool in allowed_tools:
+                if tool in neighbor.allowed_tools():
+                    g.add_edge(region.nodes[tool], neighbor.nodes[tool], weight=1)
+
+    path_length = nx.dijkstra_path_length(g, source=cave[Point(0, 0)].nodes['T'], target=cave[target].nodes['T'])
+    return path_length
+
+
 if __name__ == '__main__':
     entries = read_raw_entries('input.txt')
     depth = int(entries[0].split()[1])
     x, y = entries[1].split()[1].split(',')
     target = Point(int(x), int(y))
-    risk = solve_22(depth, target)
+    risk = solve_22_part_1(depth, target)
     print('Risk: {}'.format(risk))
+
+    time = solve_22_part_2(depth, target)
+    print('Total distance in minutes: {}'.format(time))
--- y2018/d22/test_solve_22.py
+++ y2018/d22/test_solve_22.py
@@ -1,10 +1,15 @@
 from unittest import TestCase, skip
 
 from helpers.helpers import Point
-from y2018.d22.puzzle_22 import solve_22
+from y2018.d22.puzzle_22 import solve_22_part_1, solve_22_part_2
 
 
 class TestSolve_22(TestCase):
-    def test_example(self):
-        r = solve_22(510, Point(10, 10))
+    def test_example_part_1(self):
+        r = solve_22_part_1(510, Point(10, 10))
         self.assertEqual(114, r)
+
+    def test_example_part_2(self):
+        r = solve_22_part_2(510, Point(10, 10))
+        self.assertEqual(45, r)
+
