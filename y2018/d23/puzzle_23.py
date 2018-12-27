66476530ef8d78b22591367cc1389bff7fac97fe
Troy Unverdruss <troy@unverdruss.net>
2018-12-27 12:37:22 -0500

    Part 2 solved with z3 solver

--- y2018/d23/puzzle_23.py
+++ y2018/d23/puzzle_23.py
@@ -1,7 +1,8 @@
 import re
-from typing import List
-
+from typing import List, Tuple
 from helpers.helpers import Point3d, read_raw_entries, manhattan_distance_3d
+
+import z3
 
 
 class Nanobot:
@@ -11,6 +12,9 @@ class Nanobot:
 
     def __repr__(self):
         return repr('{},{}'.format(self.loc, self.radius))
+
+    def loc_tuple(self):
+        return (self.loc.x, self.loc.y, self.loc.z)
 
 
 def solve_23(nanobots: List[Nanobot]):
@@ -23,6 +27,57 @@ def solve_23(nanobots: List[Nanobot]):
             in_range += 1
 
     return in_range
+
+def search_point(point: Point3d, bots: List[Nanobot]):
+    found_bots = []
+    for b in bots:
+        if manhattan_distance_3d(point, b.loc) <= b.radius:
+            found_bots.append(b)
+
+    return found_bots
+
+def z3abs(n):
+    return z3.If(n >= 0, n, -n)
+
+
+def z3distance(a: Tuple, b: Tuple):
+    return z3abs(a[0] - b[0]) + \
+           z3abs(a[1] - b[1]) + \
+           z3abs(a[2] - b[2])
+
+
+def solve_23_part_2(nanobots: List[Nanobot]):
+    print('Starting up')
+    origin = (0, 0, 0)
+    x, y, z, in_range = z3.Ints('x y z in_range')
+
+    in_range = in_range * 0
+    print('Vars declared')
+
+    for bot in nanobots:
+        in_range += z3.If(z3distance(bot.loc_tuple(), (x, y, z)) <= bot.radius, 1, 0)
+
+    print('In range calc prep done')
+    optimizer = z3.Optimize()
+
+    print('Created optimizer, setting max/min functions')
+    optimizer.maximize(in_range)
+    optimizer.minimize(z3distance(origin, (x, y, z)))
+
+    print('Checking optimizer')
+    optimizer.check()
+
+    print('Getting model')
+    model = optimizer.model()
+
+    print('Returning values')
+    origin_loc = Point3d(0, 0, 0)
+    found_loc = Point3d(model[x].as_long(), model[y].as_long(), model[z].as_long())
+    distance_to_found_loc = manhattan_distance_3d(origin_loc, found_loc)
+    bots_at_loc = search_point(found_loc, nanobots)
+
+    print('Distance from {} to {}: {}. Bots in range: {}'.format(origin_loc, found_loc, distance_to_found_loc, len(bots_at_loc)))
+    return distance_to_found_loc
 
 
 def parse_input(input):
@@ -42,3 +97,10 @@ if __name__ == '__main__':
 
     in_range = solve_23(nanobots)
     print('Part 1, in range: {}'.format(in_range))
+
+    distance = solve_23_part_2(nanobots)
+    print('Distance to closest point: {}'.format(distance))
+
+    # 87900620 too low
+    # 88353407 too low
+    # 100985898 ?
