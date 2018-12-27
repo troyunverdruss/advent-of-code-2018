9134d31e0aa1f86a86816d83e713267063f36fe4
Troy Unverdruss <troy@unverdruss.net>
2018-12-27 11:18:07 -0500

    test cases for part 2 of day 23

--- y2018/d23/test_solve_23.py
+++ y2018/d23/test_solve_23.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from y2018.d23.puzzle_23 import parse_input, solve_23
+from y2018.d23.puzzle_23 import parse_input, solve_23, solve_23_part_2
 
 
 class TestSolve_23(TestCase):
@@ -8,3 +8,13 @@ class TestSolve_23(TestCase):
         nanobots = parse_input('test-input.txt')
         in_range = solve_23(nanobots)
         self.assertEqual(7, in_range)
+
+    def test_mine_1(self):
+        nanobots = parse_input('my-test-input.txt')
+        in_range = solve_23_part_2(nanobots)
+        self.assertEqual(7, in_range)
+
+    def test_example_part_2(self):
+        nanobots = parse_input('test-input-part-2.txt')
+        distance = solve_23_part_2(nanobots)
+        self.assertEqual(36, distance)
